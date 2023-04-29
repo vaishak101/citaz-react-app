8c77eb95c6b014a8cc5fe7ead9f283ad23b10ffc
Vaishak Nair <nairvaishak101@gmail.com>
2023-04-29 20:08:18 +0530

    Animation fixes and hero fix for mob

--- src/components/sections/Hero/Hero.js
+++ src/components/sections/Hero/Hero.js
@@ -1,17 +1,23 @@
 import Header from './../../common/Header/Header';
-import HeroImg from './../../../assets/img/hero.jpg'
-import "./Hero.css"
+import HeroImg from './../../../assets/img/hero.jpg';
+import { useEffect } from 'react';
+import "./Hero.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Hero() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <section className="hero_wrapper">
       <div className="container">
         <Header />
-        <div className="hero">
-          <div className="img_wrap">
+        <div className="hero" >
+          <div className="img_wrap" data-aos="flip-right" data-aos-duration="1500">
             <img src={HeroImg} className="hero__img" alt="Hero" />
           </div>
-          <div className="hero__text-box">
+          <div className="hero__text-box" data-aos="flip-left" data-aos-duration="1500">
             <h1 className="heading-primary">Your Ideal Banking Partner</h1>
             <p className="paragraphy">A fresh approach to banking !</p>
           </div>
